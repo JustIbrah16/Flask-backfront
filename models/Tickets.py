d2f6477acce184b326b987818408adc961baf736
Ibrahim <practicante3@red5g.co>
2024-12-18 16:43:17 -0500

    vista de archivos y arreglos necesarios

--- models/Tickets.py
+++ models/Tickets.py
@@ -1,4 +1,5 @@
 from utils.db import db
+from models.Users import Usuarios
 
 class Tickets(db.Model):
     __tablename__ = 'tickets'
@@ -6,7 +7,13 @@ class Tickets(db.Model):
     titulo = db.Column(db.String(255), nullable=False)
     comentario = db.Column(db.Text, nullable=True)
     fk_proyecto = db.Column(db.Integer, db.ForeignKey('proyectos.id'), nullable=False)
-    fk_usuario = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  
+    fk_usuario = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    proyecto = db.relationship('Proyectos', backref='tickets', lazy=True)
+    usuario = db.relationship('Usuarios', backref='tickets', lazy=True)
+    archivos = db.relationship('Adjuntos', backref='tickets', lazy=True)
+    
+ 
 
 class Adjuntos(db.Model):
     __tablename__ = 'ticket_archivos'  
--- routes/base_tickets.py
+++ routes/base_tickets.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, session, jsonify, request
+from flask import Blueprint, session, jsonify, request, send_from_directory
 from services.roles_queries import RolesQueries
 from services.tickets_queries import TicketsQueries
 from services.proyecto_queries import ProyectosQueries
 from models.Tickets import Tickets
+from models.proyectos import Proyectos
 import os
+
 
 base_tickets = Blueprint('base_tickets', __name__)
 
@@ -25,7 +27,21 @@ def acceso_base_tickets():
         return jsonify({"message": "No se encontraron tickets"}), 404
 
     tickets_json = [
-        {"id": ticket.id, "titulo": ticket.titulo, "comentario": ticket.comentario, "proyecto_id": ticket.fk_proyecto, "usuario_id": ticket.fk_usuario}
+        {
+            "id": ticket.id,
+            "titulo": ticket.titulo,
+            "comentario": ticket.comentario,
+            "proyecto": ticket.proyecto.nombre,
+            "usuario": ticket.usuario.nombre,
+            "archivos":[
+                {
+                    "id": adjunto.id,
+                    "nombre_archivo": adjunto.nombre_archivo,
+                    "ruta_archivo": adjunto.ruta_archivo
+                }
+                for adjunto in ticket.archivos
+            ]
+        }
         for ticket in tickets
     ]
     
@@ -40,46 +56,63 @@ def crear_ticket():
     usuario_id = session.get('user_id')
     if not usuario_id:
         return jsonify({"error": "Usuario no autenticado"}), 401
-    
+
     if not RolesQueries.tiene_permiso(usuario_id, 'Crear Tickets'):
         return jsonify({"error": "No tiene permisos para crear tickets"}), 403
-    
+
     data = request.form
     titulo = data.get('titulo')
     comentario = data.get('comentario')
-    fk_proyecto = data.get('proyecto_id')
+    nombre_proyecto = data.get('nombre_proyecto')  
 
-    if not titulo or not fk_proyecto:
+    if not titulo or not nombre_proyecto:
         return jsonify({"error": "Faltan campos requeridos"}), 400
-    
-    proyecto = ProyectosQueries.obtener_proyectos(usuario_id, proyecto_id=fk_proyecto)
+
+    proyecto = Proyectos.query.filter_by(nombre=nombre_proyecto).first()
+
     if not proyecto:
-        return jsonify({"error": "El proyecto no existe o no está asociado a la compañia"}), 403
-    proyecto = proyecto[0]
-    
+        return jsonify({"error": f"No se encontró un proyecto con el nombre '{nombre_proyecto}'"}), 404
+
     archivos_adjuntos = []
     archivos = request.files.getlist('archivos')
-    total_size = sum(archivo.content_length for archivo in archivos) 
+    total_size = sum(archivo.content_length for archivo in archivos)
 
     if total_size > 10 * 1024 * 1024:  
-        return jsonify({"error": "El tamaño máximo total es de 10MB"}), 400 
- 
+        return jsonify({"error": "El tamaño máximo total es de 10MB"}), 400
+
     for archivo in archivos:
         archivo_ruta = os.path.join(UPLOAD_FOLDER, archivo.filename)
-        archivo.save(archivo_ruta) 
+        archivo.save(archivo_ruta)
         archivos_adjuntos.append({"nombre_archivo": archivo.filename, "ruta_archivo": archivo_ruta})
 
-    ticket = TicketsQueries.crear_ticket(  
+    ticket = TicketsQueries.crear_ticket(
         titulo=titulo,
-        comentario=comentario, 
-        fk_proyecto=fk_proyecto,  
+        comentario=comentario,
+        fk_proyecto=proyecto.id,  
         usuario_id=usuario_id,
-        archivos=archivos_adjuntos 
+        archivos=archivos_adjuntos
     )
 
-    return jsonify({ 
-        "message": "Ticket creado exitosamente", 
-        "ticket_id": ticket.id 
-    }), 201   
+    return jsonify({
+        "message": "Ticket creado exitosamente",
+        "ticket_id": ticket.id
+    }), 201
 
 
+# para probar en postman:
+# Selecciona la opción form-data en body.
+# Incluye los siguientes campos:
+# titulo (Texto): El título del ticket.
+# comentario (Texto, opcional): El comentario del ticket.
+# nombre_proyecto (Texto): El nombre del proyecto(ej: Bancoomeva).
+# archivos (Archivo, opcional): hasta 10 mb
+
+# @base_tickets.route('/archivos/<path:nombre_archivo>', methods=['GET'])
+# def servir_archivo(nombre_archivo):
+#     try:
+#         ruta_completa = os.path.join(UPLOAD_FOLDER, nombre_archivo)
+#         if not os.path.exists(ruta_completa):
+#             return jsonify({"error": "Archivo no encontrado"}), 404
+#         return send_from_directory(UPLOAD_FOLDER, nombre_archivo, as_attachment=False)
+#     except Exception as e:
+#         return jsonify({"error": f"Error al servir el archivo: {str(e)}"}), 500
