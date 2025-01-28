2d7b8bf2497326fa2f8c81a87bda430b9d7cbd59
Ibrahim <practicante3@red5g.co>
2025-01-28 16:00:57 -0500

    Update db apis

--- apis.sql
+++ apis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-01-2025 a las 23:40:52
+-- Tiempo de generación: 28-01-2025 a las 21:59:30
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
